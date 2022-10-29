b1073871fa23c7c8bca6eab78a19b077a021ee23
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-29 09:19:56 -0400

    Create index.js

---
+++ util/index.js
@@ -0,0 +1,12 @@
+var xss = require('xss');
+var options = {};
+
+var myxss = new xss.FilterXSS(options);
+
+exports.parseDate = function (date) {
+    return date.getFullYear() + '-' + date.getMonth() + 1 + '-' + date.getDay();
+};
+
+exports.xssfilter = function (input) {
+    return myxss.process(input);
+};
