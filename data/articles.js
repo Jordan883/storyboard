5ccfefc5255177dc3885aacde59211b3cebc1d96
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-29 15:55:27 -0400

    Update articles.js

--- data/articles.js
+++ data/articles.js
@@ -22,44 +22,6 @@ const {ObjectId} = require('mongodb');
 //   // return article;
 // };
 
-var fs = require('fs');
-var image = require('imageinfo');
-
-function readFileList(path, filesList) {
-        var files = fs.readdirSync(path);
-        files.forEach(function (itm, index) {
-            var stat = fs.statSync(path + itm);
-            if (stat.isDirectory()) {
-                readFileList(path + itm + "/", filesList)
-            } else {
-                var obj = {};
-                obj.path = path;
-                obj.filename = itm;
-                filesList.push(obj);
-            }    
-        })
-    }
-
- var getFiles = {
-        getFileList: function (path) {
-            var filesList = [];
-            readFileList(path, filesList);
-            return filesList;
-        },
-        getImageFiles: function (path) {
-            var imageList = [];
-            this.getFileList(path).forEach((item) => {		
-                var ms = image(fs.readFileSync(item.path + item.filename));
-				console.log(ms);
-                ms.mimeType && (imageList.push(item.filename))
-            });
-            return imageList;
-        }
-    };
-
-// getFiles.getImageFiles("XXX");
-// getFiles.getFileList("XXX");
-
 // module.exports = {
 //   createArticle
 
