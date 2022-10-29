ba0e7b8e85184be3d50dc31adea88bd272ab801e
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-29 14:48:20 -0400

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -1,12 +1,12 @@
 const Comment = require('../model/comment');
-const { CallbackModel } = require('../util')
+const { CallbackModel } = require('../model/comment')
 
     async create(ctx) {
         try {
             const data = await new Comment({...ctx.request.body }).save();
             CallbackModel(ctx, 200, 'Comment successfully created', data)
         } catch (error) {
-            CallbackModel(ctx, 500, 'error', JSON.stringify(error))
+            CallbackModel(ctx, 500, 'Error', JSON.stringify(error))
         }
     }
 
