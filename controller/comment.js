9e4bb2ecaaca925e101c95115cc6ac65ba8f7247
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-29 15:40:49 -0400

    Create comment.js

---
+++ controller/comment.js
@@ -0,0 +1,23 @@
+const Comment = require('../model/comment');
+const { CallbackModel } = require('../model/comment')
+
+class Utils {
+    CallbackModel(ctx, status, message, data) {
+        ctx.response.status = status;
+        ctx.body = {
+            code: status,
+            message: message,
+            data: data,
+        };
+    }
+}
+
+module.exports = new Utils;
+    async create(ctx) {
+        try {
+            const data = await new Comment({...ctx.request.body }).save();
+            CallbackModel(ctx, 200, 'Comment successfully created', data)
+        } catch (error) {
+            CallbackModel(ctx, 500, 'Error', JSON.stringify(error))
+        }
+    }
