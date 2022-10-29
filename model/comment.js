99a09669bc446638a53fbadf1a56d145efed810c
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-29 15:41:32 -0400

    Update comment.js

--- model/comment.js
+++ model/comment.js
@@ -5,16 +5,3 @@ const commentSchema = new Schema({
     content: { type: String, required: true },
 });
 module.exports = model('Comment', commentSchema);
-
-class Utils {
-    CallbackModel(ctx, status, message, data) {
-        ctx.response.status = status;
-        ctx.body = {
-            code: status,
-            message: message,
-            data: data,
-        };
-    }
-}
-
-module.exports = new Utils;
