117c268371213a9089785972c3837489feffe49e
Jordan883 <jfernan6@stevens.edu>
2022-10-29 15:11:56 -0400

    Added auth0 authentication/routing

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { auth } = require('express-openid-connect');
+require('dotenv').config()
 const app = express();
 const configRoutes = require('./routes');
 
-app.use(express.json());
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET_KEY,
+  baseURL: 'http://localhost:3000',
+  clientID: 'lBXZAxMM1Ba7xbTDE26VXQWCxjY21XSH',
+  issuerBaseURL: 'https://dev-kn-xijmw.us.auth0.com'
+};
 
+app.use(express.json());
+app.use(auth(config));
 configRoutes(app);
 
 app.listen(3000, () => {
