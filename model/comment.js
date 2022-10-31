eb60d66c1a0d21fae174a461182be736bf579c4d
Boralus <112648961+Yifan19970115@users.noreply.github.com>
2022-10-31 17:54:46 -0400

    Delete comment.js

--- model/comment.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
-
-const commentSchema = new Schema({
-    content: { type: String, required: true },
-});
-module.exports = model('Comment', commentSchema);
