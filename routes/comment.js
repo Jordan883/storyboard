39a616ab6bfcd3ecaa69077e56c8124b422ebf78
gitihabu <113068668+gitihabu@users.noreply.github.com>
2022-11-13 17:51:51 -0500

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data/comments');
+const func = require('../data/functions');
 var xss = require("xss");
 
 router.post(async (req, res) => {
@@ -9,6 +10,8 @@ router.post(async (req, res) => {
     if (!commentInfo.articleId || !commentInfo.articleComment) throw 'Please provide all inputs';
     const commentInfoarticleId = xss(commentInfo.articleId);
     const commentInfoarticleComment = xss(commentInfo.articleComment);
+    if (!func.checkId(commentInfoarticleId) || !(func.checkString(commentInfoarticleComment))
+        res.status(400).json({error: 'invalid inputs' });
     const comment = await data.createComment(
       commentInfoarticleId,
       commentInfoarticleComment
